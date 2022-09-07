03c68e419cdb8d44fcdaa10beed591d2880a4e00
zouzonghua <zouzonghua.cn@gmail.com>
2022-09-07 17:16:47 +0800

    feat: extend dark color link github

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./dist/**/*.html'],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          900: '#0d1116',
+          800: '#161b22',
+          700: '#21262d'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
